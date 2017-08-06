f98bb94115d1c850e836ea17fd7011684230b1ef
windmill <l20122005@live.com>
2017-08-06 21:55:34 +0800

    vue-router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import chat from '../components/chat.vue';
+import login from '../components/login.vue';
+
+Vue.use(Router);
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            name:'login',
+            component:login
+        },
+        {
+            path:'/chat',
+            name:'chat',
+            component:chat
+        }
+    ]
+})
