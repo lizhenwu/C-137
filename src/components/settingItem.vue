411a2f6ea0c7e1e68d2268949d4cd59a653eaf1a
windmill <l20122005@live.com>
2018-04-18 22:46:48 +0800

    add: 用户设置组件

---
+++ src/components/settingItem.vue
@@ -0,0 +1,25 @@
+<template>
+ <li class="setting-item normal-flex-box">
+    <span>{{title}}</span>
+    <input ref="checkbox" type="checkbox" :checked="checked" :id="id" @click="handCheck" hidden/>
+    <label :for="id">
+      <div></div>
+    </label>
+ </li>
+</template>
+<script>
+export default {
+  props: ['title', 'isChecked', 'id'],
+  data: function() {
+    return {
+      checked: this.isChecked 
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  li{
+    justify-content: space-between;
+  }
+</style>
+
--- src/components/systemSets.vue
+++ src/components/systemSets.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>system settings</div>
+  <div class="settings">
+    <ul>
+      <setting-item title="新消息桌面通知" isChecked = "checked" id="notice"></setting-item>
+      <setting-item title="新消息通知铃声" isChecked = "checked" id="notice-sound"></setting-item>
+      <setting-item title="新消息页面title通知" isChecked = "checked" id="notice-title"></setting-item>
+    </ul>
+  </div>
 </template>
 <script>
+import settingItem from './settingItem';
 export default {
-  
+  components: {settingItem}
 }
 </script>
 <style lang="less" scoped>
-
+  
 </style>
