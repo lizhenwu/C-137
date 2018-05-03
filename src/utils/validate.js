73cb0ff1d69901bd29f82edb153315e28d6f7671
windmill <l20122005@live.com>
2018-05-03 22:24:08 +0800

    fix: 正则test方法的坑

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,13 +1,14 @@
 const regForName = /^[\u4e00-\u9fa5_a-zA-Z0-9]{2,8}$/gi;
 const regForPwd = /^[a-zA-Z0-9.*]{5,12}$/gi;
 export default function(name, pwd, ctx) {
-    if(!regForName.test(name)) {
+    
+    if(name.search(regForName) === -1) {
       return ctx.$message({
         type: 'info',
         message: '用户名由2到8位字母数字汉字下划线组成'
       })
     }
-    if(!regForPwd.test(pwd)) {
+    if(pwd.search(regForPwd) === -1) {
       return ctx.$message({
         type: 'info',
         message: '密码由5到12位字母数字.*组成'
